17de078cca3161c5117438f9d05a508f287bfd4f
Kohei Enju <kohei@enjuk.jp>
2026-01-24 12:12:12 +0000

    use TC_ACT_ON for clarity

--- tc-skb-hash.bpf.c
+++ tc-skb-hash.bpf.c
@@ -1,10 +1,11 @@
 #include <linux/bpf.h>
+#include <linux/pkt_cls.h>
 #include <bpf/bpf_helpers.h>
 
 SEC("tc")
 int tc_skb_hash(struct __sk_buff *skb) {
     bpf_printk("SKB Hash: 0x%08x", skb->hash);
-    return 0;
+    return TC_ACT_OK;
 }
 
 char LICENSE[] SEC("license") = "GPL";
